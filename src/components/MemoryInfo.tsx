2f5ea593844b35cd2a4953d7800f4870fa229eab
Vasiliy <geass23621@gmail.com>
2019-12-12 18:18:02 +0200

    Feature: add modal for network selecting

--- src/components/MemoryInfo.tsx
+++ src/components/MemoryInfo.tsx
@@ -1,23 +1,43 @@
 import React, { useEffect, useState } from 'react';
 import { fetchData } from '../utils';
+import Button from '@material-ui/core/Button';
+import AlertDialogSlide from './AlertDialogSlide';
 
 const MemoryInfo: React.FC = () => {
+  const [wifiStatus, setWifiStatus] = useState('Not connected');
+  const [localIP, setLocalIP] = useState('');
   const [freeMemory, setFreeMemory] = useState('');
 
   useEffect(() => {
-    fetchData('freeMemory', 'freeMemory')
-      .then(res => setFreeMemory(res))
+    fetchData('localIP', 'wifiLocalIP', setLocalIP).finally();
+    fetchData('freeMemory', 'freeMemory', setFreeMemory).finally();
   }, []);
 
   return (
     <header>
-      <div>
+      <div className='info-block'>
+        <p>WiFi Status: { wifiStatus }</p>
+        { wifiStatus === 'Connected' && <p>Your local IP: { localIP }</p> }
+        { wifiStatus === 'Connected' ?
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={ changeWifiStatus }
+          >Disconnect</Button> : <AlertDialogSlide/>
+        }
+      </div>
+      <div className='info-block'>
         <p>Free Memory: { freeMemory }</p>
-        <button onClick={ () => fetchData('freeMemory', 'freeMemory')
-          .then(res => setFreeMemory(res)) }>Update</button>
+        <div>
+          <Button variant="contained" color="primary" onClick={ () => fetchData('freeMemory', 'freeMemory', setFreeMemory) }>Update</Button>
+        </div>
       </div>
     </header>
   );
+
+  function changeWifiStatus() {
+    wifiStatus === 'Connected' ? setWifiStatus('Not connected') : setWifiStatus('Connected');
+  }
 };
 
 export default MemoryInfo;
--- src/components/ScannerWifi.tsx
+++ src/components/ScannerWifi.tsx
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import { fetchData } from '../utils';
+import Button from '@material-ui/core/Button';
 
 const ScannerWifi: React.FC = () => {
   const [wifiStatus, setWifiStatus] = useState('Not connected');
   const [networks, setNetworks] = useState([]);
-  const [localIP, setLocalIP] = useState('');
-  const [isRefreshButtonDisabled, setRefreshButtonDisabled] = useState(false);
+  const [isRefreshButtonDisabled, setRefreshButtonDisabled] = useState(true);
 
   useEffect(() => {
     fetchData('statusWiFi', 'statusWiFi')
@@ -13,26 +13,16 @@ const ScannerWifi: React.FC = () => {
   }, []);
 
   useEffect(() => {
-    fetchData('localIP', 'wifiLocalIP')
-      .then(res => setLocalIP(res));
     if (wifiStatus !== 'Connected') {
-        setTimeout(() => {
-          fetchData('scannerWiFi', 'networks')
-            .then(res => setNetworks(res));
-      }, 2000);
+        setTimeout(() => refreshWifi(), 2000);
     }
   }, [wifiStatus]);
 
 
   return (
-      <div key={123}>
-      <p>WiFi Status: { wifiStatus }</p>
-      { wifiStatus === 'Connected' && <p>Your local IP: { localIP }</p> }
-      <button onClick={ changeWifiStatus }>
-        { wifiStatus === 'Connected' ? 'Disconnect' : 'Connect' }
-      </button>
+      <div className='scanner'>
       { wifiStatus !== 'Connected' &&
-      <button onClick={ refreshWifi } disabled={ isRefreshButtonDisabled }>Refresh</button>
+      <Button variant="contained" color="primary" onClick={ refreshWifi } disabled={ isRefreshButtonDisabled }>Refresh</Button>
       }
       { wifiStatus !== 'Connected' &&
       networks.map((network) => {
@@ -44,15 +34,10 @@ const ScannerWifi: React.FC = () => {
     </div>
   );
 
-  function changeWifiStatus() {
-    wifiStatus === 'Connected' ? setWifiStatus('Not connected') : setWifiStatus('Connected');
-  }
-
   function refreshWifi() {
     setRefreshButtonDisabled(true);
-    setTimeout(() => setRefreshButtonDisabled(false), 3000);
-    fetchData('scannerWiFi', 'networks')
-      .then(res => setNetworks(res));
+    fetchData('scannerWiFi', 'networks', setNetworks)
+      .finally(() => setTimeout(() => setRefreshButtonDisabled(false), 1500));
   }
 };
 
